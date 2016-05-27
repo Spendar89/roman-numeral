fd5976fa8187a54e770fc0654d095f8e1891bf3c
Jake Sendar <jakesendar@gmail.com>
2016-05-27 03:30:49 -0400

    add package.json and index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var MAPPINGS = [
+  { 'M': 1000 },
+  { 'D': 500 },
+  { 'C': 100 },
+  { 'L': 50},
+  { 'x': 10 },
+  { 'ix': 9 },
+  { 'v': 5 },
+  { 'iv': 4 },
+  { 'i': 1 }
+];
+
+function numToRoman (num, mappings=MAPPINGS) {
+  var romans = [];
+
+  Array.reduce(mappings, function (n, r) {
+    var key = Object.keys(r)[0];
+    var val = Object.values(r)[0];
+
+    for (let i = 0; i < Math.floor(n/val); i++) { 
+      romans.push(key) 
+    };
+
+    return n % val;
+  }, num);
+
+  return romans.join('');
+};
+
+module.exports = numToRoman;
