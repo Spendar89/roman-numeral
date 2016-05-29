6fda43cff56a13a01e0da4702a793642af4bd511
Jake Sendar <jakesendar@gmail.com>
2016-05-29 07:59:21 -0400

    sort map keys by map val

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 
 var config = require('./config.js');
 
+var sortByVal = function (obj) {
+  return function (a, b) { 
+    return obj[a] > obj[b] ? -1 : 1;
+  };
+};
+
 exports.convert = function (n, mappings) {
   var res = [],
-    mappings = mappings || config.mappings,
-    keys = Object.keys(mappings);
+      mappings = mappings || config.mappings,
+      keys = Object.keys(mappings),
+      sorted = keys.sort(sortByVal(mappings));
 
-  keys.forEach(function (key) {
+  sorted.forEach(function (key) {
     var val = mappings[key],
-      times = Math.floor(n/val);
+        times = Math.floor(n/val);
 
     for (var i = 0; i < times; i++) { 
       res.push(key); 
